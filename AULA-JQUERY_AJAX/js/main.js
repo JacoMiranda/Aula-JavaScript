fddd3ae2407aaa9e473a63501161abc4715f2e26
Jacó Miranda <jaco-miranda@hotmail.com>
2022-05-19 15:03:54 -0400

    Corrigindo a versão do bootstrap para v5 e usando $('#XXX).html para enviar para tela.

--- AULA-JQUERY_AJAX/js/main.js
+++ AULA-JQUERY_AJAX/js/main.js
@@ -1,18 +1,33 @@
 function consultaCep() {
 
     var cep = document.getElementById('cep').value;
-    var url = "https://viacep.com.br/ws/" + cep + "/json/";
-    //console.log(cep);
-    $.ajax({
-        url: url,
-        type: "GET",
-        success: function (response) {
-            console.log(response);
-            document.getElementById('logradouro').innerHTML = response.logradouro;
-            document.getElementById('bairro').innerHTML = response.bair;
-            document.getElementById('localidade').innerHTML = response.localidade;
-            document.getElementById('uf').innerHTML = response.uf;
-            
-        }
-    })
+    if(cep.length != 8){
+        alert("Informe um CEP Válido");
+    }else{
+        var url = "https://viacep.com.br/ws/" + cep + "/json/";
+        //console.log(cep);
+        $.ajax({
+            url: url,
+            type: "GET",
+            success: function (response) {
+                console.log(response);
+              if(response.erro ){
+                  alert("Informe um CEP Válido");
+              }else{
+               $("#logradouro").html(response.logradouro);
+               $('#bairro').html(response.bairro);
+               $('#localidade').html(response.localidade);
+               $('#uf').html(response.uf);
+    
+              }
+                
+            }
+        })
 }
+}
+//Outra forma de enviar os dados para tela
+// document.getElementById('cep').innerHTML = response.cep;
+            // document.getElementById('logradouro').innerHTML = response.logradouro;
+            // document.getElementById('bairro').innerHTML = response.bairro;
+            // document.getElementById('localidade').innerHTML = response.localidade;
+            // document.getElementById('uf').innerHTML = response.uf;
