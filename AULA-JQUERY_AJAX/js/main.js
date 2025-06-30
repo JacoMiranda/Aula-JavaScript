addcdb52140e59666789563a9df2390b81c7c286
jaco.santos <jaco-miranda@hotmail.com>
2025-06-30 15:36:05 -0400

    Ajustando o javascript para  nova abordagem

--- AULA-JQUERY_AJAX/js/main.js
+++ AULA-JQUERY_AJAX/js/main.js
@@ -1,29 +1,39 @@
-function consultaCep() {
+$(function(){
+    $("#resultado").hide();
+});
 
-    var cep = document.getElementById('cep').value;
-    if(cep.length != 8){
-        alert("Informe um CEP Válido");
-    }else{
+function consultaCep() {
+    var cep = document.getElementById('cep').value.replace(/\D/g, '');
+    if (cep.length != 8) {
+        alert("CEP inválido. Digite um CEP com 8 números.");
+        $("#resultado").hide();
+    } else {
         var url = "https://viacep.com.br/ws/" + cep + "/json/";
-        //console.log(cep);
         $.ajax({
             url: url,
             type: "GET",
             success: function (response) {
-                console.log(response);
-              if(response.erro ){
-                  alert("Informe um CEP Válido");
-              }else{
-               $("#logradouro").html(response.logradouro);
-               $('#bairro').html(response.bairro);
-               $('#localidade').html(response.localidade);
-               $('#uf').html(response.uf);
-    
-              }
-                
+                if (response.erro) {
+                    alert("CEP não encontrado.");
+                    $("#resultado").hide();
+                    $("#logradouro").html("");
+                    $('#bairro').html("");
+                    $('#localidade').html("");
+                    $('#uf').html("");
+                } else {
+                    $("#logradouro").html(response.logradouro);
+                    $('#bairro').html(response.bairro);
+                    $('#localidade').html(response.localidade);
+                    $('#uf').html(response.uf);
+                    $("#resultado").show();
+                }
+            },
+            error: function(){
+                alert("Erro ao consultar o serviço. Tente novamente mais tarde.");
+                $("#resultado").hide();
             }
         })
-}
+    }
 }
 //Outra forma de enviar os dados para tela
 // document.getElementById('cep').innerHTML = response.cep;
